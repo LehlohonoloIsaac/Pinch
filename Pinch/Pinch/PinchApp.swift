bf3790f950d9178c4dc511f28a113ee35835258c
Lehlohonolo Mbele <lehlohonoloisaac25@gmail.com>
2022-05-14 16:04:06 +0200

    Add Pinch project

---
+++ Pinch/Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Lehlohonolo Mbele on 2022/05/14.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
